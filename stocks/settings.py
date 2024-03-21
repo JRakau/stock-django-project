aa779bdafa549ab07a1067752c7da7bc9fc294ea
JRakau <josue.rakauskas@gmail.com>
2024-03-21 17:29:45 +0000

    add localhost settings.py

--- stocks/settings.py
+++ stocks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&a(3(=8u!zc_(aik_+=6=z^@27ucn=u=n-+q4+0*ij=y%o0zvk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
